2895188186bb04dfada320202e1e06b142fa8e51
Vatsal Panchal <vatsalpanchal4488@gmail.com>
2025-10-06 11:39:57 +0530

    add requirements

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ML-Ops-Production-Ready-ML-Project---US-Visa-Approval-Model
+
+- Flowchart: https://whimsical.com/
+- MLOps Tool: https://www.evidentlyai.com/
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Vatsal Panchal",
+    author_email="vatsalpanchal4488@gmail.com",
+    packages=find_packages()
+)
